4ac3bc80ed0b683cc44841668d0340dd3c98eb20
Idiot Programmer <50794593+NabeelMujeeb@users.noreply.github.com>
2021-03-24 18:33:15 +0500

    Only left click works in Info

--- Info.gd
+++ Info.gd
@@ -1,29 +1,28 @@
 extends Control
 
-
-
 func _on_BackButton_pressed() -> void:
 	get_tree().change_scene("res://MainMenu.tscn")
 
 
 func _on_head_bg_less_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		OS.shell_open("https://itch.io/jam/godot-wild-jam-31")
-
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			OS.shell_open("https://itch.io/jam/godot-wild-jam-31")
 
 
 func _on_ProgrammerIdiot_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		OS.shell_open("https://twitter.com/ProgrammerIdiot")
-
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			OS.shell_open("https://twitter.com/ProgrammerIdiot")
 
 
 func _on_2021_Twitter_logo__blue_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		OS.shell_open("https://twitter.com/ProgrammerIdiot")
-
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			OS.shell_open("https://twitter.com/ProgrammerIdiot")
 
 
 func _on_GitHubMark120pxplus_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		OS.shell_open("https://github.com/NabeelMujeeb/Grid-Collapse")
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			OS.shell_open("https://github.com/NabeelMujeeb/Grid-Collapse")
